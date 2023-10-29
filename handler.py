4d27d95110164bec18e84825c4c721b65804f40a
Natalia Correal <natalia.correal@uc.cl>
2023-10-28 21:07:15 -0300

    feat: allow post

--- handler.py
+++ handler.py
@@ -1,25 +1,68 @@
-import io
 import boto3
 from fpdf.fpdf import FPDF
+import uuid
+import time
+import json
 
 def lambda_handler(event, context):
-    # Create a PDF document
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Hello, this is a PDF created with FPDF", ln=True, align='C')
+    # Check if it's an HTTP OPTIONS (preflight) request
+    if event["httpMethod"] == "OPTIONS":
+        # Handle the CORS preflight request
+        headers = {
+            "Access-Control-Allow-Origin": "*",  # Or specify your allowed origins
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type",
+        }
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": json.dumps("CORS preflight request successful"),
+        }
 
-    # Save the PDF to a file
-    pdf_content = pdf.output(dest='S').encode('latin1')
+    if event["httpMethod"] == "POST":
+        # Parse the request body
+        request_body = json.loads(event["body"])
+        
+        # Access the data sent in the POST request
+        group = request_body.get("group")
+        user = request_body.get("user")
+        
+        # Create a PDF document
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.cell(200, 10, txt=f"Hello, this is a PDF created with FPDF\n{group} {user}", ln=True, align='C')
 
-    # Upload the PDF to an S3 bucket
-    bucket_name = "e1-arquisis"
-    s3_object_key = "sample.pdf"
+        # Save the PDF to a file
+        pdf_content = pdf.output(dest='S').encode('latin1')
 
-    s3 = boto3.client("s3")
+        # Upload the PDF to an S3 bucket
+        bucket_name = "e1-arquisis"
+        s3_object_key = f"pdf/{user}/pdf_{int(time.time())}_{str(uuid.uuid4())}.pdf"
 
-    try:
-        s3.put_object(Bucket=bucket_name, Key=s3_object_key, Body=pdf_content)
-        return f"PDF uploaded to S3 bucket: {bucket_name}, object key: {s3_object_key}"
-    except Exception as e:
-        return f"Error: {str(e)}"
+        s3 = boto3.client("s3")
+
+        try:
+            s3.put_object(Bucket=bucket_name, Key=s3_object_key, Body=pdf_content)
+            url = s3.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={
+                    'Bucket': "e1-arquisis",
+                    'Key': s3_object_key
+                },
+                ExpiresIn=24 * 3600
+            )
+            headers = {
+                "Access-Control-Allow-Origin": "*",  # This allows requests from any origin
+                "Content-Type": "application/json"
+            }
+            return {
+                "statusCode": 200,
+                "headers": headers,
+                "body": json.dumps({"url": url})
+            }
+        except Exception as e:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"error": str(e)})
+            }
